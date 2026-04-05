0e7c437776a6f9b4ae99f5b5e27e327ac8df75f9
ISRC <2403a51l03@sru.edu.in>
2026-04-06 00:00:38 +0530

    fix import.meta issue

--- lib/db/src/index.ts
+++ lib/db/src/index.ts
@@ -5,8 +5,8 @@ import { drizzle } from "drizzle-orm/node-postgres";
 import pg from "pg";
 import * as schema from "./schema";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
 config({ path: path.resolve(__dirname, "..", "..", "..", ".env") });
 
 const { Pool } = pg;
